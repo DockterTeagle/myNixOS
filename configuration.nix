c6f8409801fa3d1caf10bd0b53c8692182c27aa4
Dr.Teagle <chrisdockter@proton.me>
2024-04-22 10:54:10 -0500

    changed to unstable home-manager

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
 
 { config, pkgs, ... }:
 let 
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in
 {
   imports =
