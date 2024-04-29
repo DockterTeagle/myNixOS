f62108103b3895a8976be9798687f710a8301208
Dr.Teagle <chrisdockter@proton.me>
2024-04-29 18:30:34 -0500

    fixed indentation with on-the-go config

--- configuration.nix
+++ configuration.nix
@@ -245,13 +245,13 @@
   specialisation = {
     on-the-go.configuration = {
       system.nixos.tags = [ "on-the-go" ];
-    hardware.nvidia = {
-      prime.offload.enable = lib.mkForce true;
-      prime.offload.enableOffloadCmd = lib.mkForce true;
-      prime.sync.enable = lib.mkForce false;
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce true;
+        prime.offload.enableOffloadCmd = lib.mkForce true;
+        prime.sync.enable = lib.mkForce false;
+      };
     };
   };
-    };
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
