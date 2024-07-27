24fc4c9f00738bdd05861ccac6c87ba8ecc46b3f
Dr.Teagle <chrisdockter@proton.me>
2024-07-26 19:06:26 -0500

    updated hyprlock and hypridle

--- users/cdockter/configs/wayland/hypr/hyprPlugins/hypridle.nix
+++ users/cdockter/configs/wayland/hypr/hyprPlugins/hypridle.nix
@@ -23,8 +23,11 @@
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
       ];
-
     };
   };
 }
--- users/cdockter/configs/wayland/hypr/hyprPlugins/hyprlock.nix
+++ users/cdockter/configs/wayland/hypr/hyprPlugins/hyprlock.nix
@@ -2,6 +2,50 @@
 {
   programs.hyprlock = {
     enable = true;
-    settings = { };
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        grace = 300;
+        # hide_cursor = true;
+        no_fade_in = false;
+      };
+      imbackground = [
+        {
+          monitor = " ";
+          path = "/home/cdockter/OneDrive/Pictures/backgroundDefault.jpg";
+          size = 150;
+        }
+      ];
+      input-field = [
+        {
+          monitor = " ";
+          size = "200,50";
+          outline_thickness = 3;
+          dots_size = 0.33;
+          dots_spacing = 0.15;
+          dots_center = false;
+          dots_rounding = -1;
+          outer_color = "rgb(15,15,15)";
+          inner_color = "rgb(200, 200, 200)";
+          font_color = "rgb(10, 10, 10)";
+          fade_timeout = 1000;
+          fade_on_empty = true;
+          placeholder_text = " <i>Input Password...</i>";
+          rounding = -1;
+          check_color = "rgb(204, 136, 34)";
+          fail_color = "rgb(204, 34, 34)";
+          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+          # fail_timeout = 2000;
+          fail_transition = 300;
+          capslock_color = -1;
+          numlock_color = -1;
+          bothlock_color = -1;
+          position = "0,20";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
+
   };
 }
