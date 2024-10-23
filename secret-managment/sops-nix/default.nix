f11d00cdacb3ab667143c4fa401ac961cbcc882d
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-10-23 13:28:37 -0500

    trying to add the msi package to the kernel and failing miserabally

--- secret-managment/sops-nix/default.nix
+++ secret-managment/sops-nix/default.nix
@@ -6,12 +6,12 @@
   environment.systemPackages = with pkgs; [
     sops
   ];
-  sops = {
-    defaultSopsFile = ./secrets.yaml;
-    defaultSopsFormat = "yaml";
-    age.keyFile = "/home/cdockter/.config/sops/age/keys.txt";
-    secrets.example-key = { };
-  };
+  # sops = {
+  #   defaultSopsFile = ./secrets.yaml;
+  #   defaultSopsFormat = "yaml";
+  #   age.keyFile = "/home/cdockter/.config/sops/age/keys.txt";
+  #   secrets.example-key = { };
+  # };
   # programs.gnupg = {
   #   agent = {
   #     enable = true;
