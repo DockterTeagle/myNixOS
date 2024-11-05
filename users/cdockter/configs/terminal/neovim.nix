830b2d8cb30bd27093f7084549e2b091c38bb12b
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-11-04 21:59:05 -0600

    updated flake

--- users/cdockter/configs/terminal/neovim.nix
+++ users/cdockter/configs/terminal/neovim.nix
@@ -7,7 +7,7 @@
 {
   programs.neovim = {
     # package = inputs.nvimconfig.packages.${systemSettings.system}.default;
-    package = pkgs.neovim;
+    # package = pkgs.neovim;
     enable = true;
     defaultEditor = true;
     withNodeJs = true;
