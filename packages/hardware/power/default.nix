d83bcda2c128a79a4e672c31f57693cb3b09be15
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-11-13 21:49:30 -0600

    fixed the overheating and added shell init

--- packages/hardware/power/default.nix
+++ packages/hardware/power/default.nix
@@ -20,27 +20,26 @@
     #   };
     # };
     # tlp = {
-    #   enable = true;
-    #   settings = {
-    #     # TLP_DEFAULT_MODE = "BAT";
-    #     # TLP_PERSISTENT_DEFAULT = 1;
-    #     BluetoothPowerSave = 0;
-    #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-    #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-    #     PLATFORM_PROFILE_ON_AC = "performance";
-    #     PLATFORM_PROFILE_ON_BAT = "low-power";
-    #     CPU_MIN_PERF_ON_AC = 0;
-    #     CPU_MAX_PERF_ON_AC = 100;
-    #     CPU_MIN_PERF_ON_BAT = 0;
-    #     CPU_MAX_PERF_ON_BAT = 40;
-    #     CPU_BOOST_ON_BAT = 0;
-    #     CPU_BOOST_ON_AC = 1;
-    #     CPU_HWP_DYN_BOOST_ON_AC = 1;
-    #     CPU_HWP_DYN_BOOST_ON_BAT = 0;
-    #     # optional: helps save long-term battery health
-    #     START_CHARGE_THRESH_BAT1 = 40; # 40 and below it starts to charge
-    #     STOP_CHARGE_THRESH_BAT1 = 60; # 80 and above it stops charging
-    #   };
+    # settings = {
+    #   # TLP_DEFAULT_MODE = "BAT";
+    #   # TLP_PERSISTENT_DEFAULT = 1;
+    #   BluetoothPowerSave = 0;
+    #   CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    #   CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    #   PLATFORM_PROFILE_ON_AC = "performance";
+    #   PLATFORM_PROFILE_ON_BAT = "low-power";
+    #   CPU_MIN_PERF_ON_AC = 0;
+    #   CPU_MAX_PERF_ON_AC = 100;
+    #   CPU_MIN_PERF_ON_BAT = 0;
+    #   CPU_MAX_PERF_ON_BAT = 40;
+    #   CPU_BOOST_ON_BAT = 0;
+    #   CPU_BOOST_ON_AC = 1;
+    #   CPU_HWP_DYN_BOOST_ON_AC = 1;
+    #   CPU_HWP_DYN_BOOST_ON_BAT = 0;
+    #   # optional: helps save long-term battery health
+    #   START_CHARGE_THRESH_BAT1 = 40; # 40 and below it starts to charge
+    #   STOP_CHARGE_THRESH_BAT1 = 60; # 80 and above it stops charging
+    # };
     # };
     power-profiles-daemon.enable = true;
     thermald.enable = true;
