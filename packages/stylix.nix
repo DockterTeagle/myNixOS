2c9a964bfceec1321b8cad1cd1e44bb36936b5d7
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-11-28 13:18:17 -0600

    moved back to nixos undtable

--- packages/stylix.nix
+++ packages/stylix.nix
@@ -11,7 +11,7 @@
         popups = 11;
       };
       monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
         name = "JetBrainsMono Nerd Font ";
       };
     };
