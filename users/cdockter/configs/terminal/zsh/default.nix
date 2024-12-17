442b636177cf9f0b8f1ec219f601c113a002588e
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-16 20:00:06 -0600

    added nicer lsp support with zsh

--- users/cdockter/configs/terminal/zsh/default.nix
+++ users/cdockter/configs/terminal/zsh/default.nix
@@ -18,11 +18,27 @@ _: {
       initExtra =
         #bash
         ''
-          zstyle ':omz:plugins:alias-finder' autoload yes # disabled by default
-          zstyle ':omz:plugins:alias-finder' longer yes # disabled by default
-          zstyle ':omz:plugins:alias-finder' exact yes # disabled by default
-          zstyle ':omz:plugins:alias-finder' cheaper yes # disabled by default
-          export PATH=$PATH:/home/cdockter/.local/bin
+            zstyle ':omz:plugins:alias-finder' autoload yes # disabled by default
+            zstyle ':omz:plugins:alias-finder' longer yes # disabled by default
+            zstyle ':omz:plugins:alias-finder' exact yes # disabled by default
+            zstyle ':omz:plugins:alias-finder' cheaper yes # disabled by default
+            export PATH=$PATH:/home/cdockter/.local/bin
+            function sesh-sessions() {
+            {
+              exec </dev/tty
+              exec <&1
+              local session
+              session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+              zle reset-prompt > /dev/null 2>&1 || true
+              [[ -z "$session" ]] && return
+              sesh connect $session
+            }
+          }
+
+          zle     -N             sesh-sessions
+          bindkey -M emacs '\es' sesh-sessions
+          bindkey -M vicmd '\es' sesh-sessions
+          bindkey -M viins '\es' sesh-sessions
         '';
       oh-my-zsh = {
         enable = true;
@@ -38,9 +54,9 @@ _: {
       };
     };
     starship =
-      let
-        tokyo_night = builtins.fromTOML (builtins.readFile ./tokyo-night.toml);
-      in
+      # let
+      #   tokyo_night = builtins.fromTOML (builtins.readFile ./tokyo-night.toml);
+      # in
       {
         enable = true;
         enableBashIntegration = true;
