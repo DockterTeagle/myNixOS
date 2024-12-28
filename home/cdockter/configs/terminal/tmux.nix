82f083b33a2ccbe9d2b828a702b179c0fc539c0b
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-28 00:22:26 -0600

    added passthrough for tmux

--- home/cdockter/configs/terminal/tmux.nix
+++ home/cdockter/configs/terminal/tmux.nix
@@ -29,6 +29,7 @@
       #tmux
       ''
           set -g detach-on-destroy off
+          set -g allow-passthrough on
           bind -n M-Left select-pane -L
           bind -n M-Right select-pane -R
           bind -n M-Up select-pane -U
