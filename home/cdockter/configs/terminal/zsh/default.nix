2494dac0d7d82fc06d1dbf13dd0a8d16b5808839
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-29 20:12:05 -0600

    started integraiting redfs config

--- home/cdockter/configs/terminal/zsh/default.nix
+++ home/cdockter/configs/terminal/zsh/default.nix
@@ -63,7 +63,114 @@
         enableBashIntegration = true;
         enableZshIntegration = true;
         enableTransience = true;
-        # settings = tokyo_night;
+        settings =
+          let
+            inherit (builtins)
+              concatStringsSep
+              ;
+            vertical = "||";
+          in
+          {
+            add_newline = false;
+            format = concatStringsSep "" [
+              vertical
+              "$username"
+              "$hostname"
+              "$kubernetes"
+              "$vcsh"
+              "$git_branch"
+              "$git_commit"
+              "$git_state"
+              "$git_metrics"
+              "$git_status"
+              "$hg_branch"
+              "$docker_context"
+              "$package"
+              "$cmake"
+              "$dart"
+              "$deno"
+              "$direnv"
+              "$dotnet"
+              "$elixir"
+              "$elm"
+              "$erlang"
+              "$golang"
+              "$helm"
+              "$java"
+              "$julia"
+              "$kotlin"
+              "$nim"
+              "$nodejs"
+              "$ocaml"
+              "$perl"
+              "$php"
+              "$purescript"
+              "$python"
+              "$red"
+              "$ruby"
+              "$rust"
+              "$scala"
+              "$swift"
+              "$terraform"
+              "$vlang"
+              "$vagrant"
+              "$zig"
+              "$nix_shell"
+              "\${custom.mob}"
+              "$conda"
+              "$memory_usage"
+              "$aws"
+              "$gcloud"
+              "$openstack"
+              "$env_var"
+              "$crystal"
+              "$cmd_duration"
+              "$lua"
+              "$jobs"
+              "$battery"
+              "\n"
+              vertical
+              "$directory"
+              "\n"
+              vertical
+              "$status"
+              " [ $shell](bold)"
+              "$shlvl"
+              "$character"
+            ];
+            fill = {
+              symbol = "─";
+              style = "bold";
+            };
+            directory = {
+              truncation_length = 0;
+              format = " [ : $path]($style)[$read_only]($read_only_style) ";
+            };
+            direnv.disabled = false;
+            aws.symbol = " ";
+            conda.symbol = " ";
+            dart.symbol = " ";
+            directory.read_only = " ";
+            docker_context.symbol = " ";
+            elixir.symbol = " ";
+            elm.symbol = " ";
+            git_branch.symbol = " ";
+            golang.symbol = " ";
+            hg_branch.symbol = " ";
+            java.symbol = " ";
+            julia.symbol = " ";
+            memory_usage.symbol = "󰍛 ";
+            nim.symbol = " ";
+            nix_shell.symbol = " ";
+            package.symbol = "󰏗 ";
+            perl.symbol = " ";
+            php.symbol = " ";
+            python.symbol = " ";
+            ruby.symbol = " ";
+            rust.symbol = " ";
+            scala.symbol = " ";
+            swift.symbol = "ﯣ ";
+          };
       };
   };
 }
