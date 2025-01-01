b3ee16888d03baed5d743a2830831a0579121329
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-31 19:21:24 -0600

    added fzf config

---
+++ home/cdockter/configs/terminal/fzf.nix
@@ -0,0 +1,11 @@
+{
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
+    tmux = {
+      enableShellIntegration = true;
+    };
+  };
+}
