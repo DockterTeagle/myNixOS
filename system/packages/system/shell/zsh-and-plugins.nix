f4e5a7c12d9a020650d8db3959ce7f4d7174a0c4
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-01 09:54:10 -0600

    readded ghostty

--- system/packages/system/shell/zsh-and-plugins.nix
+++ system/packages/system/shell/zsh-and-plugins.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   fonts.fontconfig.enable = true;
   #TODO: not all of these belong here
   environment.systemPackages = with pkgs; [
-    # inputs.ghostty.packages.x86_64-linux.defaultghostty
+    inputs.ghostty.packages.x86_64-linux.default
     sesh
     lsof
     zsh
