23022a04157ffe5b08adf7f580208da6eb5002a3
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-01 16:12:36 -0600

    debggin

--- home/cdockter/configs/terminal/starship/default.nix
+++ home/cdockter/configs/terminal/starship/default.nix
@@ -1,8 +1,6 @@
 {
   programs.starship = {
     enable = true;
-    # enableBashIntegration = true;
-    # enableZshIntegration = true;
     enableTransience = true;
     settings = import ./settings.nix;
   };
