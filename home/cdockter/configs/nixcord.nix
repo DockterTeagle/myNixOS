d9e1f005711df0bd0eefb4425af4f63cad6bde2a
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-08 22:33:58 -0600

    update: hyrpland now uses vesktop

--- home/cdockter/configs/nixcord.nix
+++ home/cdockter/configs/nixcord.nix
@@ -4,5 +4,17 @@
     enable = true;
     discord.vencord.package = pkgs.vencord;
     vesktop.enable = true;
+    config = {
+      frameless = true;
+      plugins = {
+        blurNSFW.enable = true;
+        alwaysAnimate.enable = true;
+        anonymiseFileNames = {
+          enable = true;
+          anonymiseByDefault = true;
+          # randomisedLength = true;
+        };
+      };
+    };
   };
 }
