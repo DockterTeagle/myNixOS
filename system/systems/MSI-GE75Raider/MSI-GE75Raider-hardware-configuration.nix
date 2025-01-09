e104211b29827e95096fe9da3f5ecf89bc4b708f
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-08 20:56:19 -0600

    changed MSI-hardware

--- system/systems/MSI-GE75Raider/MSI-GE75Raider-hardware-configuration.nix
+++ system/systems/MSI-GE75Raider/MSI-GE75Raider-hardware-configuration.nix
@@ -7,20 +7,15 @@
   modulesPath,
   ...
 }:
+
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot = {
-    extraModulePackages = [ ];
-    kernelModules = [ "kvm-intel" ];
+
     initrd = {
-      luks.devices = {
-        # "luks-f0bd682b-a6cc-4b16-835a-81f3be960b74".device = "/dev/disk/by-uuid/f0bd682b-a6cc-4b16-835a-81f3be960b74";
-        # "EHDD".device = "/dev/disk/by-uuid/84989735-d8b3-4005-89d1-c03af68e7408";
-      };
-      kernelModules = [ ];
       availableKernelModules = [
         "xhci_pci"
         "ahci"
@@ -29,27 +24,11 @@
         "sd_mod"
         "rtsx_usb_sdmmc"
       ];
+      kernelModules = [ ];
     };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
   };
-  fileSystems = {
-    # "/" = {
-    #   device = "/dev/disk/by-uuid/62ff14dc-cadc-4a4a-af03-66459dca395a";
-    #   fsType = "ext4";
-    # };
-    # "/boot" = {
-    #   device = "/dev/disk/by-uuid/3BE8-556E";
-    #   fsType = "vfat";
-    #   options = [
-    #     "fmask=0022"
-    #     "dmask=0022"
-    #   ];
-    # };
-    # "/Drives/EHDD" = {
-    #   device = "/dev/disk/by-uuid/a6e7bbbd-757a-4f8d-981d-47630bfac731";
-    #   fsType = "ext4";
-    # };
-  };
-  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
