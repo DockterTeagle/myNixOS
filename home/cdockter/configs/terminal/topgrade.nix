9ef9f8cd16d3496f664112aafdc3716ab2aff23b
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-10 10:21:21 -0600

    fix: Topgrade now updates neovim and also updates flakes in parallel

--- home/cdockter/configs/terminal/topgrade.nix
+++ home/cdockter/configs/terminal/topgrade.nix
@@ -17,19 +17,25 @@
         set_title = false;
         cleanup = true;
       };
+      linux = { };
       commands = {
         # "Run garbage collection on Nix store" = "nh clean all";
         "Update All Nix Flakes" = # bash
           ''
-            bash -c 'for flake in $(fd -t f flake.nix ~/ --print0 --exclude .local| xargs -0 -n 1 dirname);do 
-                        echo "Updating flake: $flake";
-                        nix flake update --flake "$flake" ;
-                      done'
+                  bash -c 'fd -t f flake.nix ~/ --print0 --exclude .local | xargs -0 -P 4 -I {} bash -c "
+                dir=\$(dirname \"{}\");
+                echo \"Updating flake in directory: \$dir\"; 
+                nix flake update --flake \"\$dir\" || true
+            "'
           '';
-        "Upgrade nixos" = "nh os switch";
-        "Upgrade home-manager" = "nh  home switch";
+        "Upgrade NixOS" = # bash
+          ''nh os switch'';
+        "Upgrade home-manager" = # bash
+          ''nh  home switch'';
         "Update Lazy.nvim plugins" = # bash
-          ''nvim --headless -c ' Lazy! sync ' -c 'qall' '';
+          ''
+            nvim --headless -c " Lazy! sync " -c "qall" 
+          '';
       };
     };
   };
