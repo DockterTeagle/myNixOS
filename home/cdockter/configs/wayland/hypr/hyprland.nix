b3d58f13a41324062d34248ecf777f1a701e66d8
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-10 18:20:42 -0600

    tryin

--- home/cdockter/configs/wayland/hypr/hyprland.nix
+++ home/cdockter/configs/wayland/hypr/hyprland.nix
@@ -38,6 +38,12 @@
       general = {
         # layout = "hy3";
       };
+      cursor = {
+        # min_refresh_rate = 60;
+      };
+      # render = {
+      #   direct_scanout = true;
+      # };
       input = {
         monitor = [
           "eDP-1, 1920x1080@144 , 0x0, 1"
