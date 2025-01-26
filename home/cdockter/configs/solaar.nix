53f4e9ab58aa63d7545afcb015a651881dc75480
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-26 08:56:16 -0600

    replicated current solaar config

--- home/cdockter/configs/solaar.nix
+++ home/cdockter/configs/solaar.nix
@@ -1,8 +1,48 @@
 {
   home.file = {
-    # ".config/solaar/config.yaml".text =
-    #   #yaml
-    #   ''divert-keys: {82: 0, 83: 0, 86: 0, 195: 2, 196: 0}'';
+    ".config/solaar/config.yaml".text =
+      #yaml
+      ''
+        - _NAME: G502 X
+          _absent: [hi-res-scroll, lowres-scroll-mode, scroll-ratchet, smart-shift, thumb-scroll-invert, thumb-scroll-mode, report_rate_extended, pointer_speed,
+            dpi_extended, speed-change, backlight, backlight_level, backlight_duration_hands_out, backlight_duration_hands_in, backlight_duration_powered, backlight-timed,
+            led_control, led_zone_, rgb_control, rgb_zone_, brightness_control, per-key-lighting, fn-swap, reprogrammable-keys, persistent-remappable-keys, divert-keys,
+            disable-keyboard-keys, crown-smooth, divert-crown, divert-gkeys, m-key-leds, mr-key-led, multiplatform, change-host, gesture2-gestures, gesture2-divert,
+            gesture2-params, sidetone, equalizer, adc_power_management]
+          _battery: 0
+          _modelId: C09900000000
+          _sensitive: {dpi: false, hires-scroll-mode: ignore, hires-smooth-invert: ignore, hires-smooth-resolution: ignore}
+          _unitId: '36004000'
+          dpi: 800
+          hires-scroll-mode: false
+          hires-smooth-invert: false
+          hires-smooth-resolution: false
+          onboard_profiles: 1
+          report_rate: 1
+        - _NAME: MX Master 3 Wireless Mouse
+          _absent: [hi-res-scroll, lowres-scroll-mode, onboard_profiles, report_rate, report_rate_extended, pointer_speed, dpi_extended, speed-change, backlight,
+            backlight_level, backlight_duration_hands_out, backlight_duration_hands_in, backlight_duration_powered, backlight-timed, led_control, led_zone_, rgb_control,
+            rgb_zone_, brightness_control, per-key-lighting, fn-swap, persistent-remappable-keys, disable-keyboard-keys, crown-smooth, divert-crown, divert-gkeys,
+            m-key-leds, mr-key-led, multiplatform, gesture2-gestures, gesture2-divert, gesture2-params, sidetone, equalizer, adc_power_management]
+          _battery: 4096
+          _modelId: B02340820000
+          _sensitive: {change-host: false, dpi: true, hires-scroll-mode: true, hires-smooth-invert: false, hires-smooth-resolution: true, scroll-ratchet: false}
+          _serial: E9EBFD3A
+          _unitId: E9EBFD3A
+          _wpid: '4082'
+          change-host: null
+          divert-keys: {82: 0, 83: 0, 86: 0, 195: 0, 196: 0}
+          dpi: 700
+          hires-scroll-mode: false
+          hires-smooth-invert: false
+          hires-smooth-resolution: true
+          reprogrammable-keys: {80: 80, 81: 81, 82: 82, 83: 83, 86: 86, 195: 195, 196: 196}
+          scroll-ratchet: 2
+          smart-shift: 10
+          thumb-scroll-invert: false
+          thumb-scroll-mode: false
+
+      '';
     # ".config/solaar/rules.yaml".text =
     #   #yaml
     #   ''
