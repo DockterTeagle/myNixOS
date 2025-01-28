a616399c8c1083b224fb3920f0ff77bd79bd77a6
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-27 20:13:54 -0600

    changed shell/default.nix

--- system/packages/systems/shared/terminal/shell/default.nix
+++ system/packages/systems/shared/terminal/shell/default.nix
@@ -1,5 +1,15 @@
+{ pkgs, ... }:
 {
-  imports = [
-    ./zsh-and-plugins.nix
+  fonts.fontconfig.enable = true;
+  environment.systemPackages = with pkgs; [
+    sesh
+    unzip
+    zip
+    lazygit
+    zathura
+    ventoy-full # for writing ISO images
+    # vim-full
+    feh
+    man-db
   ];
 }
