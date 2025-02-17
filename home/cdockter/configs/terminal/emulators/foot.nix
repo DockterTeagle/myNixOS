e03e6d9e8ce7d5607d9f07300b22021b5ae97907
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-17 10:30:55 -0600

    added alacritty

--- home/cdockter/configs/terminal/emulators/foot.nix
+++ home/cdockter/configs/terminal/emulators/foot.nix
@@ -1,7 +1,6 @@
 {
   programs.foot = {
     enable = true;
-    server.enable = true;
     settings = {
       cursor = {
         style = "beam";
--- home/cdockter/configs/terminal/emulators/kitty.nix
+++ home/cdockter/configs/terminal/emulators/kitty.nix
@@ -1,10 +1,8 @@
 {
   programs.kitty = {
     enable = true;
-    # font.name = cdockterSettings.font;
     font.size = 15;
     settings = {
-      # "background_opacity" = 1;
       confirm_os_window_close = 0;
       enable_audio_bell = "no";
     };
