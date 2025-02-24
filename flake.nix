9811b46295ba0e20fc8c53fd8acb608d09df5490
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-24 00:05:05 -0600

    refactor and remove trufflehog excludes

--- flake.nix
+++ flake.nix
@@ -57,8 +57,9 @@
     nh.url = "github:viperML/nh";
     #Used exclusively for firefox
     nur.url = "github:nix-community/NUR";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     # firefox-addons = {
-    zen-browser.url = "github:0xc000022070/zen-browser-flake"; # url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    # url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     yazi.url = "github:sxyazi/yazi";
@@ -237,7 +238,6 @@
               deadnix.enable = true;
               trufflehog = {
                 enable = true;
-                excludes = [""];
               };
             };
           };
