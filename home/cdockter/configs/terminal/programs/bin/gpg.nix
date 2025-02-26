deff461627b689307b7b1b5d70eaf8179dc4b76f
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-02-26 14:37:09 -0600

    chore: continued work on gpg

--- home/cdockter/configs/terminal/programs/bin/gpg.nix
+++ home/cdockter/configs/terminal/programs/bin/gpg.nix
@@ -1,4 +1,7 @@
 {cdockterSettings, ...}: {
+  services.gpg-agent = {
+    enable = true;
+  };
   programs.gpg = {
     enable = true;
     homedir = "${cdockterSettings.homeDirectory}/.config/gnupg";
