5b3889bada2b891ce91cc96607559505903336b5
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-27 12:14:14 -0600

    feat: fixed  gpu

--- system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/GPU/default.nix
+++ system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/GPU/default.nix
@@ -44,8 +44,9 @@
       nvidiaSettings = true;
       forceFullCompositionPipeline = true;
       prime = {
+        offload.enable = true;
         allowExternalGpu = true;
-        sync.enable = true;
+        reverseSync.enable = true;
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
