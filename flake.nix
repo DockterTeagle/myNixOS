cf8e33df953f18380ef34b24d1363d964693b462
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-02-27 00:25:58 -0600

    chore: update flake.lock and condense flake

--- flake.nix
+++ flake.nix
@@ -72,87 +72,9 @@
     flake-parts,
     self,
     ...
-  } @ inputs: let
-    nixosSettings = {
-      system = "x86_64-linux";
-      hostName = "nixos";
-      timezone = "America/Chicago";
-      WSL = false;
-    };
-    nixosWSLSettings = {
-      inherit (nixosSettings) system timezone;
-      hostName = "NixOS-WSL";
-      WSL = true;
-    };
-    selectSettings = name:
-      if name == "wsl"
-      then nixosWSLSettings
-      else nixosSettings;
-    nixosProfile = builtins.getEnv "NIXOS_POFILE";
-    systemSettings = selectSettings (
-      if nixosProfile != ""
-      then nixosProfile
-      else "nixos"
-    );
-    pkgs = import nixpkgs {
-      inherit (systemSettings) system;
-      config = {
-        nvidia.acceptLicense = true;
-        allowUnfreePredicate = pkg: (builtins.elem (nixpkgs.lib.getName pkg) [
-          "nvidia-x11"
-          "discord"
-          "steam-unwrapped"
-          "steam"
-          "nvidia_driver"
-          "xow_dongle-firmware"
-          "obsidian"
-          "spotify"
-          "intel-ocl"
-          "nvidia-settings"
-          "fakespot-fake-reviews-amazon"
-          "onetab"
-          "nvidia-persistenced"
-        ]);
-        allowSubstitutes = false;
-      };
-      overlays = with inputs; [
-        neovim-nightly-overlay.overlays.default
-        nixpkgs-wayland.overlay
-        nh.overlays.default
-        hyprpanel.overlay
-        yazi.overlays.default
-        alejandra.overlay
-      ];
-    };
-    SystemModules = with inputs; [
-      ./configuration.nix
-      hyprland.nixosModules.default
-      solaar.nixosModules.default
-      lanzaboote.nixosModules.lanzaboote
-      stylix.nixosModules.stylix
-      disko.nixosModules.disko
-      sops-nix.nixosModules.sops
-      nix-gaming.nixosModules.pipewireLowLatency
-      nix-gaming.nixosModules.platformOptimizations
-      nixos-wsl.nixosModules.default
-    ];
-    cdockterSettings = {
-      username = "cdockter";
-      description = "Christopher Ryan Dockter";
-      email = "65212972+DockterTeagle@users.noreply.github.com";
-      wm = "hyprland";
-      term = "ghostty";
-      editor = "nvim";
-      font = "JetBrainsMono NF";
-      nerdfont = "jetbrains-mono";
-      homeDirectory = "/home/cdockter";
-      cursorPackage = pkgs.bibata-cursors;
-      cursorName = "Bibata-Modern-Ice";
-      cursorSize = 24;
-    };
-  in
+  } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = nixpkgs.lib.systems.flakeExposed;
       debug = true;
       imports = with inputs; [
         git-hooks-nix.flakeModule
@@ -167,37 +89,108 @@
         treefmt = import ./treefmt.nix {inherit inputs' self pkgs;};
         devenv = import ./devenv.nix {inherit inputs' pkgs;};
       };
-      flake = {
-        nixosConfigurations = {
-          wsl = nixpkgs.lib.nixosSystem {
-            inherit pkgs;
-            specialArgs = {
-              inherit
-                inputs
-                systemSettings
-                cdockterSettings
-                ;
-            };
-            modules = SystemModules;
-          };
-          nixos = nixpkgs.lib.nixosSystem {
-            inherit pkgs;
-            specialArgs = {
-              inherit
-                inputs
-                systemSettings
-                cdockterSettings
-                ;
-            };
-            modules = SystemModules;
-          };
+      flake = let
+        nixosSettings = {
+          system = "x86_64-linux";
+          hostName = "nixos";
+          timezone = "America/Chicago";
+          WSL = false;
         };
+        nixosWSLSettings =
+          nixosSettings
+          // {
+            hostName = "NixOS-WSL";
+            WSL = true;
+          };
+
+        # Determine the profile: if the environment variable is empty, default to "nixos"
+        profile = let
+          p = builtins.getEnv "NIXOS_POFILE";
+        in
+          if p != ""
+          then p
+          else "nixos";
+        systemSettings =
+          if profile == "wsl"
+          then nixosWSLSettings
+          else nixosSettings;
+
+        pkgs = import nixpkgs {
+          inherit (systemSettings) system;
+          config = {
+            nvidia.acceptLicense = true;
+            allowUnfreePredicate = pkg:
+              builtins.elem (nixpkgs.lib.getName pkg) [
+                "nvidia-x11"
+                "discord"
+                "steam-unwrapped"
+                "steam"
+                "nvidia_driver"
+                "xow_dongle-firmware"
+                "obsidian"
+                "spotify"
+                "intel-ocl"
+                "nvidia-settings"
+                "fakespot-fake-reviews-amazon"
+                "onetab"
+                "nvidia-persistenced"
+              ];
+            allowSubstitutes = false;
+          };
+          overlays = with inputs; [
+            neovim-nightly-overlay.overlays.default
+            nixpkgs-wayland.overlay
+            nh.overlays.default
+            hyprpanel.overlay
+            yazi.overlays.default
+            alejandra.overlay
+          ];
+        };
+
+        SystemModules = with inputs; [
+          ./configuration.nix
+          hyprland.nixosModules.default
+          solaar.nixosModules.default
+          lanzaboote.nixosModules.lanzaboote
+          stylix.nixosModules.stylix
+          disko.nixosModules.disko
+          sops-nix.nixosModules.sops
+          nix-gaming.nixosModules.pipewireLowLatency
+          nix-gaming.nixosModules.platformOptimizations
+          nixos-wsl.nixosModules.default
+        ];
+
+        cdockterSettings = {
+          username = "cdockter";
+          description = "Christopher Ryan Dockter";
+          email = "65212972+DockterTeagle@users.noreply.github.com";
+          wm = "hyprland";
+          term = "ghostty";
+          editor = "nvim";
+          font = "JetBrainsMono NF";
+          nerdfont = "jetbrains-mono";
+          homeDirectory = "/home/cdockter";
+          cursorPackage = pkgs.bibata-cursors;
+          cursorName = "Bibata-Modern-Ice";
+          cursorSize = 24;
+        };
+
+        # Common arguments for nixosConfigurations
+        specialArgs = {inherit inputs systemSettings cdockterSettings;};
+      in {
+        nixosConfigurations = builtins.listToAttrs (map (name: {
+          inherit name;
+          value = nixpkgs.lib.nixosSystem {
+            inherit pkgs;
+            inherit specialArgs;
+            modules = SystemModules;
+          };
+        }) ["wsl" "nixos"]);
+
         homeConfigurations = {
           cdockter = home-manager.lib.homeManagerConfiguration {
             inherit pkgs;
-            extraSpecialArgs = {
-              inherit inputs cdockterSettings systemSettings;
-            };
+            extraSpecialArgs = specialArgs;
             modules = with inputs; [
               ./home/cdockter/home.nix
               stylix.homeManagerModules.stylix
