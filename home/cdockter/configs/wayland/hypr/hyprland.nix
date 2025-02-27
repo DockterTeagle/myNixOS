f5c7f9aa1ebdbaff269cbc40755ff78f2616e558
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-02-27 12:41:42 -0600

    feat: add specailworkspace in hyprland

--- home/cdockter/configs/wayland/hypr/hyprland.nix
+++ home/cdockter/configs/wayland/hypr/hyprland.nix
@@ -17,6 +17,12 @@
         submap = prefix
         bind = SHIFT,z,fullscreen,
         bind = ,z,fullscreen,1
+        bind =$mainMod,comma,submap,special
+        submap = special
+        bind = ,F4,exec,[workspace special:bt silent] ghostty -e btop
+        bind = CONTROL,l,togglespecialworkspace,bt
+        bind = ,escape,submap,prefix
+        submap = prefix
         # add more later
         #also add an indicator somewhere
         bind = ,escape,submap,reset
