dc3913d4c1f83ee5103f8d9ab648c7d8aa061034
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-06 19:49:28 -0600

    refactor: start the hyprland config rewrite

--- home/cdockter/configs/wayland/hypr/default.nix
+++ home/cdockter/configs/wayland/hypr/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./hyprpanel.nix
-    ./hyprland.nix
+    ./hyprland
     ./hyprEcosystem
   ];
 }
---
+++ home/cdockter/configs/wayland/hypr/hyprland/decorations/default.nix
@@ -0,0 +1,27 @@
+{lib, ...}: {
+  wayland.windowManager.hyprland = {
+    settings = {
+      decoration = {
+        rounding = 2;
+        dim_inactive = true;
+        dim_strength = 0.1;
+        shadow = {
+          enabled = true;
+          range = 30;
+          render_power = 3;
+          color = lib.mkForce "0x66000000";
+        };
+        blur = {
+          enabled = true;
+          size = 6;
+          passes = 2;
+          ignore_opacity = 1;
+          xray = true;
+          new_optimizations = 1;
+          noise = 3.0e-2;
+          contrast = 1.0;
+        };
+      };
+    };
+  };
+}
