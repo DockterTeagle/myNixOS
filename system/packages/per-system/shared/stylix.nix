e81d8652cb66a660ba72243edc253712a28ea1ba
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-07 09:11:02 -0600

    fix: use correct path

--- system/packages/per-system/shared/stylix.nix
+++ system/packages/per-system/shared/stylix.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   stylix = {
-    image = ../../../assets/pictures/wallpaper-dread.png;
+    image = ../../../../assets/pictures/wallpaper-dread.png;
     # image = pkgs.fetchurl {
     #   url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nixos-wallpaper-catppuccin-mocha.png";
     #   sha256 = "7e6285630da06006058cebf896bf089173ed65f135fbcf32290e2f8c471ac75b";
