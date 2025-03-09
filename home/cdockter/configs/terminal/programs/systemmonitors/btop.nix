4cd56c298aa0ec38ba5b771fce59f1e4cc7948c4
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-08 18:49:44 -0600

    feat: make btop show gpu

--- home/cdockter/configs/terminal/programs/systemmonitors/btop.nix
+++ home/cdockter/configs/terminal/programs/systemmonitors/btop.nix
@@ -1,9 +1,12 @@
-{
+{pkgs, ...}: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override {cudaSupport = true;};
     settings = {
       vim_keys = true;
       update_ms = 100;
+      shown_boxes = "proc cpu mem net gpu1";
+      proc_tree = true;
     };
   };
 }
