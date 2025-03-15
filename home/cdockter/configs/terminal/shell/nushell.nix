bf2cf22cb212d197c4662ab51210be889b75c1df
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-14 20:12:09 -0500

    feat: add shell aliases to nushell

--- home/cdockter/configs/terminal/shell/nushell.nix
+++ home/cdockter/configs/terminal/shell/nushell.nix
@@ -1,8 +1,37 @@
 {pkgs, ...}: {
   programs.nushell = {
+    shellAliases = {
+      ta = "tmux attach";
+      g = "git";
+      ga = "git add";
+      gaa = "git add --all";
+      gc = "git commit";
+      gl = "git pull";
+      gp = "git push";
+      gcl = "git clone";
+      update = "topgrade";
+      grtr = "cd \"$(git rev-parse --show-toplevel || echo .)\"";
+      cdi = "__zoxide_zi";
+      man = "batman";
+      cd = "__zoxide_z";
+    };
     enable = true;
+    envFile.text =
+      #nu
+      ''
+        $env.TRANSIENT_PROMPT_COMMAND = ">"
+      '';
+    settings = {
+      buffer_editor = "nvim";
+      show_banner = false;
+    };
     plugins = with pkgs.nushellPlugins; [
       highlight
+      # gstat
+      # formats
+      # polars
+      # query
+      # skim
     ];
   };
 }
