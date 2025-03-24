7e4bfd95172a0213f55ba8ff49a48bd085c0191e
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-23 20:59:02 -0500

    feat: add windowrules

--- home/cdockter/configs/wayland/hypr/hyprland/windowrules.nix
+++ home/cdockter/configs/wayland/hypr/hyprland/windowrules.nix
@@ -1,10 +1,11 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "tile,^(Firefox)$"
-      "float,^(blueman-manager)$"
-    ];
-    windowrulev2 = [
+      #smart Gaps
+      # "bordersize 0, floating:0, onworkspace:w[tv1]"
+      # "rounding 0, floating:0, onworkspace:w[tv1]"
+      # "bordersize 0, floating:0, onworkspace:f[1]"
+      # "rounding 0, floating:0, onworkspace:f[1]"
       "idleinhibit focus, class:^steam_app_.*$"
       "idleinhibit fullscreen,class:^(firefox|chromium|chrome)$, title:^(.*YouTube.*)$"
     ];
--- home/cdockter/configs/wayland/hypr/hyprland/workspaces.nix
+++ home/cdockter/configs/wayland/hypr/hyprland/workspaces.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     workspace = [
       "w[tv1],gapsout:0,gapsin:0"
+      "f[1],gapsout:0,gapsin:0"
     ];
   };
 }
