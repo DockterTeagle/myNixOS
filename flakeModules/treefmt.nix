0766f319b0f959eff49a433bd20082462ba05dfc
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-23 20:59:56 -0500

    feat: add nufmt

--- flakeModules/treefmt.nix
+++ flakeModules/treefmt.nix
@@ -1,6 +1,14 @@
-{self', ...}: {
+{
+  inputs',
+  self',
+  ...
+}: {
   flakeFormatter = true;
   programs = {
+    nufmt = {
+      enable = true;
+      package = inputs'.nufmt.packages.default;
+    };
     alejandra = {
       enable = true;
     };
