1999a116185f3921b69fe0e7efb2724c1eb541ab
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-04-23 17:35:26 -0500

    chore: switch to dprint

--- flakeModules/treefmt.nix
+++ flakeModules/treefmt.nix
@@ -1,18 +1,22 @@
 {self', ...}: {
   flakeFormatter = true;
   programs = {
+    dprint = {
+      enable = true;
+      includes = ["*.md"];
+      settings = {
+        useTabs = false;
+        incremental = true;
+        plugins = ["https://plugins.dprint.dev/markdown-0.18.0.wasm"];
+      };
+    };
     nufmt = {
       enable = true;
     };
     alejandra = {
       enable = true;
     };
-    beautysh = {
-      enable = true;
-    };
-    prettier = {
-      enable = true;
-    };
+    beautysh.enable = true;
   };
   build = {
     check = self';
