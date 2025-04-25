ed25638607ee9d6594ed67c1cf655e2a6399ba8a
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-04-24 23:43:03 -0500

    fix: remove nixpkgs-wayland, caused problems

--- home/cdockter/configs/wayland/hypr/hyprland/binds/default.nix
+++ home/cdockter/configs/wayland/hypr/hyprland/binds/default.nix
@@ -6,11 +6,7 @@
     ./shortcuts.nix
     ./submaps.nix
   ];
-  /*
-  TODO: make an indicator to tell when in prefix/ special mode,
-  also make it execute only one command at a time unless told to. Also
-  make all binds have docs and a help key
-  */
+  /**/
   wayland.windowManager.hyprland.settings = {
     bindr = [
       "SUPER_L,$mainMod,exec,rofi -show drun -show-icons run"
