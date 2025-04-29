aa53b9d1a7c0d2ec2d84adaa9542295ba2eab76d
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-04-29 17:48:17 -0500

    chore: comment out unused things

--- home/cdockter/configs/GUI/firefox.nix
+++ home/cdockter/configs/GUI/firefox.nix
@@ -4,45 +4,45 @@
   ...
 }: {
   home.packages = with inputs; [zen-browser.packages.${systemSettings.system}.twilight];
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      cdockter = {
-        extensions = {
-          settings = {};
-          # packages = with inputs.firefox-addons.packages.${systemSettings.system}; [
-          #   tree-style-tab
-          #   facebook-container
-          #   tst-more-tree-commands
-          #   cookie-autodelete
-          #   fakespot-fake-reviews-amazon
-          #   private-relay
-          #   leechblock-ng
-          #   onetab
-          #   ublock-origin
-          #   multi-account-containers
-          #   proton-pass
-          # ];
-        };
-        name = "cdockter";
-        isDefault = true;
-        settings = {
-          "privacy.trackingprotection.enabled" = true;
-          "extensions.autoDisableScope" = 0;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        };
-        search = {
-          default = "ddg";
-          force = true;
-        };
-        userChrome =
-          #css
-          ''
-            #TabsToolbar {
-                visibility: collapse !important;
-              }
-          '';
-      };
-    };
-  };
+  # programs.firefox = {
+  #   enable = true;
+  #   profiles = {
+  #     cdockter = {
+  #       extensions = {
+  #         settings = {};
+  #         # packages = with inputs.firefox-addons.packages.${systemSettings.system}; [
+  #         #   tree-style-tab
+  #         #   facebook-container
+  #         #   tst-more-tree-commands
+  #         #   cookie-autodelete
+  #         #   fakespot-fake-reviews-amazon
+  #         #   private-relay
+  #         #   leechblock-ng
+  #         #   onetab
+  #         #   ublock-origin
+  #         #   multi-account-containers
+  #         #   proton-pass
+  #         # ];
+  #       };
+  #       name = "cdockter";
+  #       isDefault = true;
+  #       settings = {
+  #         "privacy.trackingprotection.enabled" = true;
+  #         "extensions.autoDisableScope" = 0;
+  #         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  #       };
+  #       search = {
+  #         default = "ddg";
+  #         force = true;
+  #       };
+  #       userChrome =
+  #         #css
+  #         ''
+  #           #TabsToolbar {
+  #               visibility: collapse !important;
+  #             }
+  #         '';
+  #     };
+  #   };
+  # };
 }
