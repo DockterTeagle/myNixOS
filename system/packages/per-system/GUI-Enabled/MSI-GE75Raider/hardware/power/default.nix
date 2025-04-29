76149a5d713771b301b5b67112b347f97b1fe4bf
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-04-29 17:58:16 -0500

    feat: fiexd cpu

--- system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/power/default.nix
+++ system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/power/default.nix
@@ -6,18 +6,21 @@
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
+  powerManagement = {
+    cpuFreqGovernor = "performance";
+  };
   services = {
     power-profiles-daemon.enable = lib.mkForce false;
     tlp = {
       enable = true;
       settings = {
-        RUNTIME_PM_ENABLE = "01:00.0";
         BluetoothPowerSave = 0;
+        CPU_SCALING_GOVERNONR_ON_AC = "schedutil";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         PLATFORM_PROFILE_ON_AC = "performance";
         PLATFORM_PROFILE_ON_BAT = "low-power";
-        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MIN_PERF_ON_AC = 50;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 40;
@@ -28,12 +31,8 @@
         # optional: helps save long-term battery health
         START_CHARGE_THRESH_BAT1 = 40; # 40 and below it starts to charge
         STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
-        INTEL_GPU_MIN_FREQ_ON_BAT = 300;
-        INTEL_GPU_MIN_FREQ_ON_AC = 500;
-        INTEL_GPU_MAX_FREQ_ON_BAT = 1000;
-        INTEL_GPU_MAX_FREQ_ON_AC = 1150;
-        INTEL_GPU_BOOST_FREQ_ON_AC = 1150;
-        INTEL_GPU_BOOST_FREQ_ON_BAT = 1150;
+        PCIE_ASPM_ON_AC = "performance";
+        #removed settings below as intel gpu shouldn't be that important
       };
     };
     thermald.enable = true;
