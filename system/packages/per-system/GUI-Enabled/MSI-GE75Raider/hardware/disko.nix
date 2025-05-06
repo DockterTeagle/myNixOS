f4640a44d036d5b1fd307597a96a69045bff6525
DockterTeagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-06 00:32:29 +0000

    feat:Update disko.nix

--- system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/disko.nix
+++ system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/disko.nix
@@ -1,6 +1,25 @@
 {
   disko.devices = {
     disk = {
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+		boot= {
+name = "boot";
+size = "1M";
+type="EF00";
+};
+ESP = {name = "ESP";size = "500M"; type = "EF00";content = {
+	type = "filesystem"; format = "vfat"; mountpoint = "/boot"; mountoptions = ["umask=077"];};};
+luks = {size = "100%"; content = {
+	type = "luks"; name = "crypted-ssd"; extraOpenArgs = []; passwordFile = "/tmp/secret-key"; settings = {
+allowDiscards = true; crypttabExtraOpts = [ "fido2-deviced=auto" "token-timeout=10" ];};
+content = {type = "lvm_pv"; vg = "root_vg";};};};};
+          };
+        };
       EHDD = {
         type = "disk";
         device = "/dev/sda";
@@ -13,71 +32,23 @@
               content = {
                 type = "luks";
                 name = "cryptHDD";
-                settings.allowDiscards = true;
-                extraOpenArgs = [
-                  "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                settings = {
-                  crypttabExtraOpts = [
-                    "fido2-device=auto"
-                    "token-timeout=10"
-                  ];
-                };
-                # passwordFile = "";
+                settings={allowDiscards = true;
+crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];};
+};
+
+                 passwordFile = "/tmp/secret.key";
                 content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/Drives/EHDD";
-                };
-              };
-            };
-          };
-        };
-      };
-      nvme0n1 = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              name = "ESP";
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "umask=0077"
-                ];
-              };
-            };
-            luks = {
-              size = "100%";
-              label = "luksBase";
-              content = {
-                type = "luks";
-                name = "cryptroot";
-                settings.allowDiscards = true;
-                extraOpenArgs = [
-                  "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                settings = {
-                  crypttabExtraOpts = [
-                    "fido2-device=auto"
-                    "token-timeout=10"
+			type = "lvm_pv"; vg = "root_vg";};};};};};
+lvm_vg = {root_vg = {type = "lvm_vg";lvs={root={size="100%FREE";content={type="btrfs";extraArgs=["-f"];
+subvolumes = {"/root" = {mountpoint = "/"};"/persist" = {mountOptions = ["compress=zstd" "subvol=persist""noatime"];mountpoint = "/persist";};
+  "/nix" = {
+                  mountOptions = [
+                    "compress=zstd"
+                    "subvol=nix"
+                    "noatime"
                   ];
+                  mountpoint = "/nix";
                 };
-                # passwordFile = "";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
                 };
               };
             };
