01fb31c69d1adef684bddc906b6990133c7c6e44
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-06 18:38:19 -0500

    feat: fuck impermance for now

--- system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/disko.nix
+++ system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/disko.nix
@@ -1,121 +1,83 @@
-_: {
+{
   disko.devices = {
     disk = {
-      main = {
-        device = "/dev/nvme0n1";
+      EHDD = {
         type = "disk";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF00";
+            luks = {
+              size = "100%";
+              label = "luksEHDD";
+              content = {
+                type = "luks";
+                name = "cryptHDD";
+                settings.allowDiscards = true;
+                extraOpenArgs = [
+                  "--allow-discards"
+                  "--perf-no_read_workqueue"
+                  "--perf-no_write_workqueue"
+                ];
+                settings = {
+                  crypttabExtraOpts = [
+                    "fido2-device=auto"
+                    "token-timeout=10"
+                  ];
+                };
+                # passwordFile = "";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/Drives/EHDD";
+                };
+              };
             };
+          };
+        };
+      };
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
             ESP = {
               name = "ESP";
-              size = "1G";
+              size = "500M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
-              };
-            };
-            swap = {
-              size = "24G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
+                mountOptions = [
+                  "umask=0077"
+                ];
               };
             };
             luks = {
               size = "100%";
+              label = "luksBase";
               content = {
                 type = "luks";
-                name = "crypted-main";
-                extraOpenArgs = [];
-                passwordFile = "/tmp/secret.key";
+                name = "cryptroot";
+                settings.allowDiscards = true;
+                extraOpenArgs = [
+                  "--allow-discards"
+                  "--perf-no_read_workqueue"
+                  "--perf-no_write_workqueue"
+                ];
                 settings = {
-                  allowDiscards = true;
                   crypttabExtraOpts = [
                     "fido2-device=auto"
                     "token-timeout=10"
                   ];
                 };
+                # passwordFile = "";
                 content = {
-                  type = "lvm_pv";
-                  vg = "root_vg";
-                };
-              };
-            };
-          };
-        };
-      };
-
-      extra = {
-        device = "/dev/nvme1n1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted-extra";
-                extraOpenArgs = [];
-                passwordFile = "/tmp/secret.key";
-                settings = {
-                  allowDiscards = true;
-                  crypttabExtraOpts = [
-                    "fido2-device=auto"
-                    "token-timeout=10"
-                  ];
-                };
-                content = {
-                  type = "lvm_pv";
-                  vg = "root_vg";
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-
-    lvm_vg = {
-      root_vg = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "btrfs";
-              extraArgs = ["-f"];
-
-              subvolumes = {
-                "/root" = {
+                  type = "filesystem";
+                  format = "ext4";
                   mountpoint = "/";
-                };
-
-                "/persist" = {
-                  mountOptions = [
-                    "compress=zstd"
-                    "subvol=persist"
-                    "noatime"
-                  ];
-                  mountpoint = "/persist";
-                };
-
-                "/nix" = {
-                  mountOptions = [
-                    "compress=zstd"
-                    "subvol=nix"
-                    "noatime"
-                  ];
-                  mountpoint = "/nix";
                 };
               };
             };
