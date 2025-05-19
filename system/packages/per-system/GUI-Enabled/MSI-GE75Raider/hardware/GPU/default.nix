9edaeac025934f1f54216664881b9504fe29bc4d
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-19 16:31:40 -0500

    chore: remove unused comment

--- system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/GPU/default.nix
+++ system/packages/per-system/GUI-Enabled/MSI-GE75Raider/hardware/GPU/default.nix
@@ -13,7 +13,6 @@
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
-      # AQ_DRM_DEVICES = "/dev/dri/card1";
     };
     sessionVariables = {
       NIXOS_OZONE_WL = 1;
