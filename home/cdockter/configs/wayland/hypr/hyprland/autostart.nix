643c3daf5a5c6a9f19e3e505fdfd701ba2e69b3c
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-19 16:15:34 -0500

    feat: add steam back to autostart and remove pyprland

--- home/cdockter/configs/wayland/hypr/hyprland/autostart.nix
+++ home/cdockter/configs/wayland/hypr/hyprland/autostart.nix
@@ -1,13 +1,13 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "pyprland"
       "wl-paste --type text --watch cliphist store"
       "wl-paste --type image --watch cliphist store"
       "systemctl --user enable --now hyprpolkitagent.service"
       "systemctl --user enable --now hyprsunset.service"
       "uwsm app -- $term"
       "uwsm app -- zen-twilight"
+      "uwsm app -- steam"
       "uwsm app -- vesktop"
     ];
   };
