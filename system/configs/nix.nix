a28179079843a72c0bc985f2c90e97447c59d03d
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-20 08:13:52 -0500

    chore: update flake.lock

---
+++ system/configs/nix.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  nix = {
+    package = pkgs.lix;
+    channel.enable = false;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "repl-flake"
+        "no-url-literals"
+      ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://yazi.cachix.org"
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-substituters = [
+        "https://hyprland.cachix.org"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://yazi.cachix.org"
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+      auto-optimise-store = true;
+    };
+  };
+}
