6ba41f46b8f5db74955a21fb610c8995a1705efc
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-20 09:32:17 -0500

    fix: update secrets

--- home/cdockter/secrets/default.nix
+++ home/cdockter/secrets/default.nix
@@ -4,6 +4,9 @@
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/cdockter/.config/sops/age/keys.txt";
     secrets = {
+      nixAccessTokens = {
+        mode = "444";
+      };
       SSHKEY = {
         path = "/home/cdockter/.ssh/id_ed25519";
       };
