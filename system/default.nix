f9a0bf5f1079027c62bcbaf2bfa90e5d6e297006
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-26 15:35:59 -0500

    refactor: move nix config to configs dir

--- system/default.nix
+++ system/default.nix
@@ -1,56 +1,17 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   pkgs,
   cdockterSettings,
   systemSettings,
   config,
-  # lib,
   ...
 }: {
   # Imports
   imports = [
     ./packages
     ./secrets
+    ./configs
+    # ./services
   ];
-  # Nix settings
-  nix = {
-    channel.enable = false;
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-        "pipe-operators"
-        "no-url-literals"
-      ];
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://yazi.cachix.org"
-        "https://nix-gaming.cachix.org"
-        "https://ghostty.cachix.org"
-      ];
-      trusted-substituters = [
-        "https://hyprland.cachix.org"
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://yazi.cachix.org"
-        "https://nix-gaming.cachix.org"
-        "https://ghostty.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-      ];
-      auto-optimise-store = true;
-    };
-  };
 
   # Time and Locale
   time.timeZone = "America/Chicago";
@@ -111,7 +72,6 @@
 
   # Programs
   programs = {
-    # nix-ld.dev.enable = true;
     gnupg = {
       # agent = {
       #   enable = true;
@@ -137,6 +97,6 @@
     autoUpgrade.enable = false;
 
     # NixOS Release Version
-    stateVersion = "23.11"; # Did you read the comment?
+    stateVersion = "25.05"; # Did you read the comment?
   };
 }
