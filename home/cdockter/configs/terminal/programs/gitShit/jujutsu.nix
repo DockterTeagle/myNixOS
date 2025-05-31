de0c1dd2509a414d9903f7b0a817315d6160ec2b
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-31 17:15:26 -0500

    fix: remove solaar and also add signing

--- home/cdockter/configs/terminal/programs/gitShit/jujutsu.nix
+++ home/cdockter/configs/terminal/programs/gitShit/jujutsu.nix
@@ -2,6 +2,13 @@
   programs.jujutsu = {
     enable = true;
     settings = {
+      git = {
+        sign-on-push = true;
+      };
+      signing = {
+        behavior = "own";
+        backend = "gpg";
+      };
       user = {
         inherit (cdockterSettings) email;
         name = "Dr.Teagle";
