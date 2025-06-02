f31e009c8b148a5bbeedb53e43c502f30eea4afb
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-06-02 12:46:01 -0500

    feat: simplify submaps

--- home/cdockter/configs/wayland/hypr/hyprland/binds/submaps.nix
+++ home/cdockter/configs/wayland/hypr/hyprland/binds/submaps.nix
@@ -7,14 +7,13 @@
       bind = SHIFT,z,fullscreen,
       bind = ,z,fullscreen,1
       bindl = $mainMod,l,exec,uwsm stop
-      bind =$mainMod,comma,submap,special
+      bind =,comma,submap,special
       submap = special
       bind = ,F4,exec,[workspace special:bt silent] ghostty -e btop
       bind = CONTROL,l,togglespecialworkspace,bt
       bind = ,escape,submap,prefix
       submap = prefix
       # add more later
-      #also add an indicator somewhere
       bind = ,escape,submap,reset
       # bind = ,catchall, submap, reset
       submap = reset
