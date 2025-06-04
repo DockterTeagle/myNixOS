0aa178a944527628495b3e8c1bdbb89f4a7d9ec2
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-06-04 11:22:19 -0500

    refactor: make flake smaller

---
+++ globals/default.nix
@@ -0,0 +1,8 @@
+{inputs, ...}: let
+  systemConfig = import ./system {inherit inputs;};
+  inherit (systemConfig) pkgs systemSettings;
+  homeConfig = import ./home {inherit pkgs;};
+  inherit (homeConfig) cdockterSettings;
+in {
+  inherit pkgs systemSettings cdockterSettings;
+}
---
+++ globals/home/default.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  cdockterSettings = {
+    username = "cdockter";
+    description = "Christopher Ryan Dockter";
+    email = "65212972+DockterTeagle@users.noreply.github.com";
+    wm = "hyprland";
+    term = "ghostty";
+    editor = "nvim";
+    font = "JetBrainsMono NF";
+    nerdfont = pkgs.nerd-fonts.jetbrains-mono;
+    homeDirectory = "/home/cdockter";
+    cursorPackage = pkgs.bibata-cursors;
+    cursorName = "Bibata-Modern-Ice";
+    cursorSize = 24;
+    theme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
+  };
+}
