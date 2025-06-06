41db8bdeba18519a72d2fdf80b4b8b47d5831f83
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-06-06 11:24:13 -0500

    feat: add yaziPlugins

--- home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
+++ home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
@@ -1,7 +1,11 @@
-{
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+    initLua = ./yaziInit.lua;
+    plugins = {
+      inherit (pkgs.yaziPlugins) git;
+    };
     settings = {
       mgr = {
         show_hidden = true;
