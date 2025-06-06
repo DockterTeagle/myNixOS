59e6d82b637430bd3374e75200a4e47b5aa971f3
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-06-06 11:24:13 -0500

    feat: yazi plugins

--- home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
+++ home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
@@ -4,9 +4,21 @@
     shellWrapperName = "y";
     initLua = ./yaziInit.lua;
     plugins = {
-      inherit (pkgs.yaziPlugins) git;
+      inherit (pkgs.yaziPlugins) git full-border;
     };
     settings = {
+      plugin.prepend_fetchers = [
+        {
+          id = "git";
+          name = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          name = "*/";
+          run = "git";
+        }
+      ];
       mgr = {
         show_hidden = true;
         sort_by = "natural";
