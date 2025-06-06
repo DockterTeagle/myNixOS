cc4778a04454a4b93f11568f3461d2dbc1da3659
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-06-06 11:24:13 -0500

    feat: fuzzel

--- home/cdockter/configs/wayland/rofi/default.nix
+++ home/cdockter/configs/wayland/rofi/default.nix
@@ -3,16 +3,16 @@
   cdockterSettings,
   ...
 }: {
-  programs.rofi = {
-    enable = true;
-    package = pkgs.rofi-wayland;
-    extraConfig = {
-      show = "drun"; # Set the default mode to drun
-      run-command = "uwsm app -- {cmd}";
-      run-shell-command = "ghostty -e {cmd}";
-      kb-mode-next = "Alt+Tab";
-    };
-    terminal = "${cdockterSettings.term}";
-    location = "center";
-  };
+  # programs.rofi = {
+  #   enable = true;
+  #   package = pkgs.rofi-wayland;
+  #   extraConfig = {
+  #     show = "drun"; # Set the default mode to drun
+  #     run-command = "uwsm app -- {cmd}";
+  #     run-shell-command = "ghostty -e {cmd}";
+  #     kb-mode-next = "Alt+Tab";
+  #   };
+  #   terminal = "${cdockterSettings.term}";
+  #   location = "center";
+  # };
 }
