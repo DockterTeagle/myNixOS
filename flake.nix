6f49a716e6fe5bc9b64c835053ace68b61a236b0
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-07-14 19:01:21 -0500

    revert|feat: use nixpkgs wayland again, also formatting

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   description = "My nixos flake";
   inputs = {
-    /*
-    Core Nix Packages and Flakes
-    */
+    # Core Nix Packages and Flakes
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; # IMPORTANT
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+    # Flake-parts and modules
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "/home/cdockter/Documents/Projects/CodingProjects/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     #Boot
@@ -46,7 +46,7 @@
       url = "github:nix-community/nh";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    yazi.url = "github:sxyazi/yazi"; #uses cache so dont override ;
+    yazi.url = "github:sxyazi/yazi"; # uses cache so dont override ;
     ghostty.url = "github:ghostty-org/ghostty";
     ## Neovim Configurations and Overlays
     neovim-nightly-overlay = {
@@ -55,7 +55,7 @@
 
     # Wayland and GUI Tools
     ##Hyprland
-    hyprland.url = "github:hyprwm/Hyprland"; #uses cachix so won't override
+    hyprland.url = "github:hyprwm/Hyprland"; # uses cachix so won't override
     stylix.url = "github:nix-community/stylix";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
@@ -64,64 +64,70 @@
     };
     nix-gaming.url = "github:fufexan/nix-gaming";
   };
-  outputs = {
-    nixpkgs,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    {
+      nixpkgs,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       debug = true;
       imports = with inputs; [
         treefmt-nix.flakeModule
         devenv.flakeModule
       ];
-      perSystem = {
-        imports = [./flakeModules];
-      };
-      flake = let
-        config = import ./globals {inherit inputs;};
-        inherit (config) pkgs systemSettings cdockterSettings;
+      perSystem.imports = [ ./flakeModules ];
+      flake =
+        let
+          config = import ./globals { inherit inputs; };
+          inherit (config) pkgs systemSettings cdockterSettings;
 
-        # Common arguments for nixosConfigurations
-        specialArgs = {inherit inputs systemSettings cdockterSettings;};
-      in {
-        nixosConfigurations = builtins.listToAttrs (map (name: {
-          inherit name;
-          value = nixpkgs.lib.nixosSystem {
-            inherit pkgs;
-            inherit specialArgs;
-            modules = with inputs; [
-              ./system
-              chaotic.nixosModules.default
-              solaar.nixosModules.default
-              stylix.nixosModules.stylix
-              disko.nixosModules.disko
-              sops-nix.nixosModules.sops
-              nix-gaming.nixosModules.pipewireLowLatency
-              nix-gaming.nixosModules.platformOptimizations
-              lanzaboote.nixosModules.lanzaboote
-            ];
-          };
-        }) ["wsl" "nixos"]);
+          # Common arguments for nixosConfigurations
+          specialArgs = { inherit inputs systemSettings cdockterSettings; };
+        in
+        {
+          nixosConfigurations = builtins.listToAttrs (
+            map
+              (name: {
+                inherit name;
+                value = nixpkgs.lib.nixosSystem {
+                  inherit pkgs;
+                  inherit specialArgs;
+                  modules = with inputs; [
+                    ./system
+                    chaotic.nixosModules.default
+                    solaar.nixosModules.default
+                    stylix.nixosModules.stylix
+                    disko.nixosModules.disko
+                    sops-nix.nixosModules.sops
+                    nix-gaming.nixosModules.pipewireLowLatency
+                    nix-gaming.nixosModules.platformOptimizations
+                    lanzaboote.nixosModules.lanzaboote
+                  ];
+                };
+              })
+              [
+                "wsl"
+                "nixos"
+              ]
+          );
 
-        homeConfigurations = {
-          cdockter = inputs.home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs =
-              specialArgs
-              // {
+          homeConfigurations = {
+            cdockter = inputs.home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              extraSpecialArgs = specialArgs // {
                 inherit pkgs;
               };
-            modules = with inputs; [
-              ./home/cdockter
-              zen-browser.homeModules.twilight
-              stylix.homeModules.stylix
-              sops-nix.homeManagerModules.sops
-              nix-index-database.homeModules.nix-index
-            ];
+              modules = with inputs; [
+                ./home/cdockter
+                zen-browser.homeModules.twilight
+                stylix.homeModules.stylix
+                sops-nix.homeManagerModules.sops
+                nix-index-database.homeModules.nix-index
+              ];
+            };
           };
         };
-      };
     };
 }
