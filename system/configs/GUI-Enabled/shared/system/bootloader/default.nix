0ba54d011078d6980b7efafe86b996e8007233da
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-07-27 22:40:12 -0500

    feat: use cachyos

--- system/configs/GUI-Enabled/shared/system/bootloader/default.nix
+++ system/configs/GUI-Enabled/shared/system/bootloader/default.nix
@@ -17,16 +17,7 @@
     };
 
     initrd.luks.reusePassphrases = false;
-    kernelParams = [
-      "boot.shell_on_fail"
-      "systemd.log_target=console"
-      #other stuff
-      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-      "bluetooth.disable_ertm=0"
-      "snd-hda-intel.model=auto"
-      "snd-hda-intel.probe_mask=1"
-      "threadirqs"
-    ];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos;
+    # kernelPackages = pkgs.linuxPackages_latest;
   };
 }
