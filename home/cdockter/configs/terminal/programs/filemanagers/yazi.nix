4c712ba1ed934b7d3609bd328cdc2a1e98a86b57
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-08-15 13:07:07 -0500

    feawt: add more yazPlugins

--- home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
+++ home/cdockter/configs/terminal/programs/filemanagers/yazi.nix
@@ -5,13 +5,30 @@
     shellWrapperName = "y";
     initLua = ./yaziInit.lua;
     plugins = {
-      inherit (pkgs.yaziPlugins) git full-border mount;
+      inherit (pkgs.yaziPlugins)
+        git
+        full-border
+        mount
+        smart-paste
+        smart-filter
+        starship
+        ;
     };
     keymap = {
       mgr.prepend_keymap = [
         {
+          on = "F";
+          run = "plugin smart-filter";
+          desc = "Smart  Filter";
+        }
+        {
           on = "M";
           run = "plugin mount";
+        }
+        {
+          on = "p";
+          run = "plugin smart-paste";
+          desc = "Paste into the hovered dir or CWD";
         }
       ];
     };
