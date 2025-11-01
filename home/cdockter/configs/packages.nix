4f0a90d0a14dd9e9aa2c6b92e12a14067b7ee82c
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-11-01 12:28:03 -0500

    chore: remove packages that have modules

--- home/cdockter/configs/packages.nix
+++ home/cdockter/configs/packages.nix
@@ -3,19 +3,14 @@
   #TODO: check if any of me are modules
   home.packages = with pkgs; [
     axel
-    bc
     rsync
     ddcutil
-    gnome-keyring
-    networkmanager
     kdePackages.plasma-nm
     kdePackages.bluedevil
     kdePackages.dolphin
     kdePackages.systemsettings
-    hyprshot
     nwg-displays
     imagemagick
     songrec
-    translate-shell
   ];
 }
