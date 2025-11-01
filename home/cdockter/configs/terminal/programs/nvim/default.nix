20152526004da49f48b41ff427e6d9dba557d9d5
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-11-01 12:28:03 -0500

    fix(attempt1): try to get sqlite working for snacks.nvim

--- home/cdockter/configs/terminal/programs/nvim/default.nix
+++ home/cdockter/configs/terminal/programs/nvim/default.nix
@@ -8,11 +8,22 @@
     vimdiffAlias = true;
     defaultEditor = true;
     withNodeJs = true;
+    extraLuaConfig = ''vim.g.sqlite_clib_path = ${pkgs.sqlite.out}/lib/sqlite.so'';
   };
   #NOTE: NONE OF ME ARE MODULES --check date 08/22/25
   home.packages = with pkgs; [
-    git-absorb
+    # git-absorb
     tree-sitter
-    # xdotool # for vimtex
+    sqlite
+    xdotool
+    /*
+      sqlite_orm
+      sqlitestudio
+      sqlite-web
+      luajitPackages.sqlite
+      sqlitecpp
+      xdotool # for vimtex
+    */
+
   ];
 }
